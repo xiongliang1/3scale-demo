1f0151698ca14a4649940eb6ef45f2d12ad494b8
xiongliang <chaoyang.beijing>
2020-12-23 11:28:06 +0800

    端口修改ddsadasdas

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/tt/{id}")
     public String hello(@PathVariable( value = "id") String id){
-        return  "this is a test....my host....######.....yyysjgihfdigfdigiu实际"+ id;
+        return  "this is a test....my host....######.....yyysjgihfdigfdigiu实际ssss"+ id;
     }
 
 
